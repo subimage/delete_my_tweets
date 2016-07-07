870ba278023459ca6df92a3df5bd8b0c2296f508
Seth <stb@subimage.com>
2016-07-07 13:25:56 -0700

    Refactored into a class for maintainability

--- delete-my-tweets.rb
+++ delete-my-tweets.rb
@@ -5,52 +5,78 @@ require 'bundler/setup'
 require 'twitter' 
 require 'yaml'
 
-config = YAML::load_file('config.yml')
-fc = config.first
-username = fc['username']
+class DeleteMyTweets
+  def initialize
+    yaml_config = YAML::load_file('config.yml')
+    fc = yaml_config.first
+    @username = fc['username']
+    @num_times_slept = 0
 
-puts "\nDELETING TWEETS FOR: #{username}"
-puts "(Ctrl-C to stop)\n\n"
- 
-Twitter.configure do |config|
-  config.consumer_key       = fc['consumer_key']
-  config.consumer_secret    = fc['consumer_secret']
-  config.oauth_token        = fc['oauth_token']
-  config.oauth_token_secret = fc['oauth_secret']
-end
-
-t_id = 0
-while t_id != nil do
-  t_id = nil
-  tweets = Twitter.user_timeline(username, {
-      :count => 200, 
-      :include_entities => true, 
-      :trim_user => true, 
-      :include_rts => true
-    }
-  )
-  puts "statuses=#{tweets.length}"
-  if tweets.length == 0
-    t_id = 1
-    puts "Sleeping for 10 minutes..."
-    sleep (60*10)
-  end
-  tweets.each do |t|
-    #puts t.inspect
-    #next unless t.reply?
-    t_id = t['id']
-    begin 
-      if Twitter.status_destroy(t_id)
-        puts "D: #{t['text']}"
-      else
-        puts "\n!!! #{t_id}\n"
-      end
-    rescue Twitter::Error::ClientError => e
-      puts e.message
-      sleep 5
-    rescue Twitter::Error::NotFound => e
-      puts e.message
-      sleep 5
+    puts "\nDELETING TWEETS FOR: #{@username}"
+    puts "(Ctrl-C to stop)\n\n"
+     
+    Twitter.configure do |config|
+      config.consumer_key       = fc['consumer_key']
+      config.consumer_secret    = fc['consumer_secret']
+      config.oauth_token        = fc['oauth_token']
+      config.oauth_token_secret = fc['oauth_secret']
     end
   end
+
+  def run
+    t_id = 0
+    while t_id != nil && 1 >= @num_times_slept do
+      t_id = nil
+      fetch_tweets
+      puts "statuses=#{@tweets.length}"
+      if @tweets.length == 0
+        t_id = 1
+        wait_for_api
+      else
+        t_id = destroy_tweets
+      end
+    end
+  end
+
+  def fetch_tweets
+    @tweets = Twitter.user_timeline(@username, {
+        :count => 200, 
+        :include_entities => true, 
+        :trim_user => true, 
+        :include_rts => true
+      }
+    )
+  end
+
+  def destroy_tweets
+    last_tweet_id = nil
+    @tweets.each do |t|
+      last_tweet_id = t['id']
+      begin 
+        if Twitter.status_destroy(last_tweet_id)
+          puts "D: #{t['text']}"
+        else
+          puts "\n!!! #{last_tweet_id}\n"
+        end
+      rescue Twitter::Error::ClientError => e
+        puts e.message
+        sleep 5
+      rescue Twitter::Error::NotFound => e
+        puts e.message
+        sleep 5
+      end
+    end
+    return last_tweet_id
+  end
+
+  def wait_for_api
+    puts "No more tweets to delete..."
+    puts "Sleeping for 10 minutes to see if we can find more."
+    puts "(CTRL-C to exit)\n"
+    @num_times_slept += 1
+    sleep (60*10)
+  end
+
 end
+
+DeleteMyTweets.new.run
