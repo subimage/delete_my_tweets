6593986766c20633d3b4a6c3c368768bbb1451af
Seth <seth@subimage.com>
2018-08-12 12:19:35 -0700

    Should probably clean up these ugly variable names if I'm going to put this thing on github

--- delete_my_tweets.rb
+++ delete_my_tweets.rb
@@ -2,49 +2,49 @@
 
 require 'rubygems'
 require 'bundler/setup'
-require 'twitter' 
+require 'twitter'
 require 'yaml'
 
 class DeleteMyTweets
   def initialize
     yaml_config = YAML::load_file('config.yml')
-    fc = yaml_config.first
-    @username = fc['username']
+    config = yaml_config.first
+    @username = config['username']
     @num_times_slept = 0
-    @delete_words = fc['search_words_to_delete']
+    @delete_words = config['search_words_to_delete']
     @delete_words ||= []
 
     puts "\nDELETING TWEETS FOR: #{@username}"
     puts "(Ctrl-C to stop)\n\n"
-     
+
     Twitter.configure do |config|
-      config.consumer_key       = fc['consumer_key']
-      config.consumer_secret    = fc['consumer_secret']
-      config.oauth_token        = fc['oauth_token']
-      config.oauth_token_secret = fc['oauth_secret']
+      config.consumer_key       = config['consumer_key']
+      config.consumer_secret    = config['consumer_secret']
+      config.oauth_token        = config['oauth_token']
+      config.oauth_token_secret = config['oauth_secret']
     end
   end
 
   def run
-    t_id = 0
-    while t_id != nil && 1 >= @num_times_slept do
-      t_id = nil
+    tweet_id = 0
+    while tweet_id != nil && 1 >= @num_times_slept do
+      tweet_id = nil
       fetch_tweets
       puts "statuses=#{@tweets.length}"
       if @tweets.length == 0
-        t_id = 1
+        tweet_id = 1
         wait_for_api
       else
-        t_id = destroy_tweets
+        tweet_id = destroy_tweets
       end
     end
   end
 
   def fetch_tweets
     @tweets = Twitter.user_timeline(@username, {
-        :count => 200, 
-        :include_entities => true, 
-        :trim_user => true, 
+        :count => 200,
+        :include_entities => true,
+        :trim_user => true,
         :include_rts => true
       }
     )
@@ -56,21 +56,21 @@ class DeleteMyTweets
       #puts t.inspect
       if @delete_words.length > 0
         found_keyword_to_delete = false
-        match_text = t['text'].downcase 
-        if t.attrs[:quoted_status] 
+        match_text = t['text'].downcase
+        if t.attrs[:quoted_status]
           match_text += ' ' + t.attrs[:quoted_status][:text].downcase
         end
         @delete_words.each do |w|
           if match_text.include?(w)
             found_keyword_to_delete = true
-            break 
+            break
           end
         end
         # only delete tweets with keywords
         next unless found_keyword_to_delete
       end
       last_tweet_id = t['id']
-      begin 
+      begin
         if Twitter.status_destroy(last_tweet_id)
           puts "D: #{t['text']}"
         else
